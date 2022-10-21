54b4548f6710512104a165ea239965f013340cce
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-21 20:24:42 +0300

    feat: basic form layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,22 @@
+import { TextField, Typography, styled } from '@mui/material';
 import { Layout } from 'components/layout';
 import React from 'react';
 
+const FileUpload = styled('div')`
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  max-width: 500px;
+  gap: 20;
+`;
+
 export function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <FileUpload>
+        <Typography variant="h5">Pickup файл</Typography>
+        <TextField type="file" />
+      </FileUpload>
+    </Layout>
+  );
 }
