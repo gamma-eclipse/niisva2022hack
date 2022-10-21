605ac7d6c7cfc3c495a8a089264ca0a7ebf527d9
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-21 20:12:11 +0300

    feat: create some mocks

---
+++ src/mocks/dataMock.js
@@ -0,0 +1,28 @@
+export const dataMock = [
+  {
+    id: 1,
+    packages: 142,
+    traffic: 4314314,
+    protocol: 'https',
+    host: '192.168.214.54',
+    target: 'https://localgoi.com',
+    classification: {
+      purpose: 'messager',
+      name: 'chrome',
+      // ?
+    },
+  },
+  {
+    id: 2,
+    packages: 65,
+    traffic: 543256235,
+    protocol: 'http',
+    host: '192.168.214.54',
+    target: 'https://nillkiggers.ua',
+    classification: {
+      purpose: 'social',
+      name: 'telegram',
+      // ?
+    },
+  },
+];
