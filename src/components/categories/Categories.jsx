a8a4bb75dad797f7ef8207057cfd6ab15ce58928
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 17:00:09 +0300

    fix: accordion styles

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,22 +1,36 @@
-import { Button, Collapse } from '@mui/material';
+import { Button, Collapse, Typography, styled } from '@mui/material';
 import { analyzeStore } from 'app/store/analyzeStore';
 import { observer } from 'mobx-react-lite';
 import { useState } from 'react';
 
 import { CategoriesTable } from './CategoriesTable';
 
+const Wrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const Head = styled('div')`
+  display: flex;
+  gap: 25px;
+  align-items: flex-end;
+`;
+
 function Categories() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <Button variant="contained" onClick={() => setOpen(!open)}>
-        Анализ категорий
-      </Button>
+    <Wrapper>
+      <Head>
+        <Typography variant="h4">Категории</Typography>
+        <Button variant="contained" onClick={() => setOpen(!open)}>
+          {open ? 'Закрыть' : 'Открыть'}
+        </Button>
+      </Head>
       <Collapse in={open} timeout="auto">
         {analyzeStore.categories && <CategoriesTable categories={analyzeStore.categories} />}
       </Collapse>
-    </div>
+    </Wrapper>
   );
 }
 
