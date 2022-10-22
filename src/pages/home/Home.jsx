a19ad90a14fbc93f89aeb45ac248b0483f7210ac
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 11:33:13 +0300

    feat: create upload animation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,17 +6,19 @@ import { observer } from 'mobx-react-lite';
 
 import { homeStore } from './store';
 
-const Wrapper = styled('div')`
+const Wrapper = styled('div', { shouldForwardProp: (p) => p !== 'dataFetched' })`
   display: flex;
   align-items: center;
   flex-direction: column;
   gap: 30px;
+  transition: all 0.7s ease-in-out;
+  padding-top: ${(props) => (props.dataFetched ? '0' : '30vh')};
 `;
 
 function Home() {
   return (
     <Layout>
-      <Wrapper>
+      <Wrapper dataFetched={homeStore.data}>
         <UploadForm />
         {homeStore.data && <GeneralAnalyzeTable analyzeResults={homeStore.data} />}
       </Wrapper>
