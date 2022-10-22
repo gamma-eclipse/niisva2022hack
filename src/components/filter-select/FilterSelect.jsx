974de6a6acc71e37508b3280d74840d7e1f147a7
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 12:30:42 +0300

    feat: extract filter select component

---
+++ src/components/filter-select/FilterSelect.jsx
@@ -0,0 +1,31 @@
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { filtersStore } from 'app/store/filtersStore';
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+function FilterSelect({ name, label, width = 150 }) {
+  return (
+    <FormControl style={{ width }}>
+      <InputLabel id={`select_${name}`}>{label}</InputLabel>
+      <Select
+        labelId={`select_${name}`}
+        value={filtersStore.applied[name]}
+        onChange={(e) => {
+          filtersStore.applied[name] = e.target.value;
+        }}
+        label={label}
+      >
+        <MenuItem selected value="">
+          Не выбрано
+        </MenuItem>
+        {filtersStore.options[name].map((v) => (
+          <MenuItem key={v} value={v}>
+            {v}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default observer(FilterSelect);
--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,5 +1,6 @@
 import { FormControl, InputLabel, MenuItem, Select, styled } from '@mui/material';
 import { filtersStore } from 'app/store/filtersStore';
+import { FilterSelect } from 'components/filter-select';
 import { observer } from 'mobx-react-lite';
 
 const Wrapper = styled('div')`
@@ -9,50 +10,10 @@ const Wrapper = styled('div')`
 `;
 
 function Filters() {
-  console.log(filtersStore.applied);
   return (
     <Wrapper>
-      <FormControl style={{ width: 150 }}>
-        <InputLabel id="select_name">Название</InputLabel>
-        <Select
-          // variant="filled"
-          labelId="select_name"
-          value={filtersStore.applied.name}
-          onChange={(e) => {
-            filtersStore.applied.name = e.target.value;
-          }}
-          label="Название"
-        >
-          <MenuItem selected value="">
-            Не выбрано
-          </MenuItem>
-          {filtersStore.options.name.map((v) => (
-            <MenuItem key={v} value={v}>
-              {v}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl style={{ width: 150 }}>
-        <InputLabel id="select_purpose">Назначение</InputLabel>
-        <Select
-          labelId="select_purpose"
-          value={filtersStore.applied.purpose}
-          onChange={(e) => {
-            filtersStore.applied.purpose = e.target.value;
-          }}
-          label="Назначение"
-        >
-          <MenuItem selected value="">
-            Не выбрано
-          </MenuItem>
-          {filtersStore.options.purpose.map((v) => (
-            <MenuItem key={v} value={v}>
-              {v}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <FilterSelect name="name" label="Название" />
+      <FilterSelect name="purpose" label="Назначение" />
     </Wrapper>
   );
 }
