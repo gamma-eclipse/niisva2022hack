e7805308c5e48ec909944cfda34e869806f32da4
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 16:55:28 +0300

    feat: create categories tables

--- src/app/store/analyzeStore.js
+++ src/app/store/analyzeStore.js
@@ -9,7 +9,11 @@ class AnalyzeStore {
     makeAutoObservable(this);
   }
 
+  CATEGORY_NAMES = ['name', 'purpose'];
+
   analyzes = null;
+
+  categories = null;
 
   get filteredAnalyzes() {
     return filtersStore.filterAnalyzeResults(this.analyzes);
@@ -26,8 +30,34 @@ class AnalyzeStore {
 
     if (data) {
       this.analyzes = data;
+      this.genCategories();
     }
   };
+
+  genCategories() {
+    const categoriesMap = {};
+
+    for (const name of this.CATEGORY_NAMES) {
+      categoriesMap[name] = {};
+    }
+
+    for (const analyze of this.analyzes) {
+      Object.entries(analyze.classification).forEach(([name, value]) => {
+        if (!categoriesMap[name][value]) {
+          categoriesMap[name][value] = {
+            packages: 0,
+            traffic: 0,
+          };
+        }
+        categoriesMap[name][value].packages += analyze.packages;
+        categoriesMap[name][value].traffic += analyze.traffic;
+      });
+    }
+
+    console.log(categoriesMap);
+
+    this.categories = categoriesMap;
+  }
 }
 
 export const analyzeStore = new AnalyzeStore();
--- src/components/categories/CategoriesTable.jsx
+++ src/components/categories/CategoriesTable.jsx
@@ -1,3 +1,52 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { toJS } from 'mobx';
+
 export function CategoriesTable({ categories }) {
-  return <div>text</div>;
+  // console.log(categories);
+  return (
+    <div>
+      {Object.entries(categories).map(([categoryName, categoryData]) => {
+        return (
+          <Accordion key={categoryName}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>{categoryName}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Название</TableCell>
+                    <TableCell>Пакетов</TableCell>
+                    <TableCell>Трафик</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Object.entries(categoryData).map(([itemName, itemData]) => {
+                    return (
+                      <TableRow key={Math.random()}>
+                        <TableCell>{itemName}</TableCell>
+                        <TableCell> {itemData.packages}</TableCell>
+                        <TableCell> {itemData.traffic}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </div>
+  );
 }
