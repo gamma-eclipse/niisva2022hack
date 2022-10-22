6ebcc051d2e8b4c082f63729e72a30f48a2a3cf3
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 22:16:19 +0300

    ref: extract helper

--- src/app/store/analyzeStore.js
+++ src/app/store/analyzeStore.js
@@ -1,5 +1,6 @@
 import { makeAutoObservable } from 'mobx';
 import { axiosMock } from 'shared/helpers/axiosMock';
+import { generateCategories } from 'shared/helpers/generateCategories';
 import { analyzesMock } from 'shared/mocks/analyzesMock';
 
 import { filtersStore } from './filtersStore';
@@ -40,28 +41,7 @@ class AnalyzeStore {
   };
 
   genCategories() {
-    const categoriesMap = {};
-
-    for (const name of this.CATEGORY_NAMES) {
-      categoriesMap[name] = {};
-    }
-
-    for (const analyze of this.analyzes) {
-      Object.entries(analyze.classification).forEach(([categoryName, categoryValue]) => {
-        if (!categoriesMap[categoryName][categoryValue]) {
-          categoriesMap[categoryName][categoryValue] = {
-            packages: 0,
-            traffic: 0,
-          };
-        }
-        categoriesMap[categoryName][categoryValue].packages += analyze.packages;
-        categoriesMap[categoryName][categoryValue].traffic += analyze.traffic;
-      });
-    }
-
-    console.log(categoriesMap);
-
-    this.categories = categoriesMap;
+    this.categories = generateCategories(this.analyzes, this.CATEGORY_NAMES);
   }
 }
 
