08c1d2e4768ebe11ffa754a64215c05625ec0902
Dispersioned <71058712+Dispersioned@users.noreply.github.com>
2022-10-22 21:55:36 +0300

    ref: cleanup

--- src/components/general-analyze-table/GeneralAnalyze.jsx
+++ src/components/general-analyze-table/GeneralAnalyze.jsx
@@ -1,22 +1,9 @@
-import { Button, Collapse, Typography, styled } from '@mui/material';
 import { analyzeStore } from 'app/store/analyzeStore';
 import { Filters } from 'components/filters';
 import { Spoiler } from 'components/spoiler';
 import { observer } from 'mobx-react-lite';
-import { useState } from 'react';
 
 import { GeneralAnalyzeTable } from './GeneralAnalyzeTable';
-
-const Wrapper = styled('div')`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
-const Head = styled('div')`
-  display: flex;
-  gap: 25px;
-  align-items: flex-end;
-`;
 
 function GeneralAnalyze() {
   return (
